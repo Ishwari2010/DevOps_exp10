53299cd76934214c8014134d26270983860db050
SHINDE ISHWARI DEVENDRA <5023155@it.fcrit.ac.in>
2026-03-24 14:20:33 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("\nHello " + name + "!");
+        System.out.println("You are " + age + " years old.");
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        sc.close();
+    }
+}
